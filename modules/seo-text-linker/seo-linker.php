e472e89c708633dbf56e1dae5433bac41a74b388
Nahman ALLOUCHE <nahmana@elementor.com>
2022-03-27 22:24:09 +0300

    finished php and front phase of feature, no preview yet.

--- modules/seo-text-linker/seo-linker.php
+++ modules/seo-text-linker/seo-linker.php
@@ -19,108 +19,43 @@ class Seo_Linker
 
 
 		add_filter( 'elementor/frontend/the_content', function($content) {
-			$settings = Plugin::$instance->kits_manager->get_active_kit_for_frontend()->get_settings_for_display();
 
-
-
-
-			// print_r($settings['underline_exist']);
-			print_r($settings['seo_repeater'][0]['text']);
-			echo "<br>";
-			print_r($settings['seo_repeater'][0]['link']['url']);
-			echo "<br>";
-			print_r($settings['seo_repeater'][0]['link']['is_external']);
-			echo "<br>";
-			print_r($settings['seo_repeater'][1]['text']);
-			echo "<br>";
-			print_r($settings['seo_repeater'][1]['link']['url']);
-			echo "<br>";
-			print_r($settings['seo_repeater'][1]['link']['is_external']);
-			echo "<br>";
-			// $this->ScanContent($content);
+			$kit = Plugin::$instance->kits_manager->get_active_kit_for_frontend();
+			$settings = $kit->get_settings_for_display();
 			
+			// If no linker set, or first default linker is not set (no url to link), return regular content:
+			if( (count($settings['seo_linker']) < 1) || (count($settings['seo_linker']) === 1 && $settings['seo_linker'][0]['link']['url'] === '') ) {
+				return $content;
+			}
 
+			// If current page is in exclude array, return regular content:
+			$page_id = get_queried_object_id();
+			$excludes = str_replace(' ', '', $settings['seo_linker_exclude_pages']);
+			$excludes_array = explode(',', $excludes);
+			if (in_array($page_id, $excludes_array)) {
+				return $content;
+			}
 
+			$new_content = $this->scan_content($content, $settings);
+			return $new_content;
 
-
-
-
-			// _____________________________________________________
-
-
-			// $xml_content = simplexml_load_string($content);
-			// print_r($xml_content);
-
-			// $str = htmlentities($content);
-		
-
-			// $bla = $this->MyHelper("bla");
-
-			// echo "<br><hr><br>";
-			// echo $bla;
-
-			
-
-			// echo "<br><hr><br>";
-			// // echo $p_content;
-			// // echo htmlentities($p_content);
-			// // print_r(htmlentities($content_array));
-			// foreach ($content_array as $key => $value) {
-			// 	echo htmlentities($value);
-			// 	echo "<br><hr><br>";
-			// }
-
-			// return  htmlentities($content);
-
-		
-				// foreach ($content_array as $value) {
-		// 	echo htmlentities($value);
-		// 	echo "<br><hr><br>";
-		// }
-
-
-			
-			// $dom = new \DomDocument();
-			// libxml_use_internal_errors(true);
-			
-			// $dom->loadHTML($content);
-			// $xpath = new \DOMXpath($dom);
-
-			// $elements = $xpath->query('//p');
-
-			// foreach ($elements as $entry) {
-			// 	echo "<br>>";
-			// 	print_r($entry->childNode);
-			// 	echo "<br><hr><br>";
-			// }
-
-			// print_r($xpath->query('//p'));
-			// var_dump($xpath->query('//p'));
-
-			// // $elements = $xpath->query('//p');
-			// // var_dump($elements);
-
-			// libxml_clear_errors();
-
-			
-
-			// var_dump($content);
-			// content of specific page
-			wp_die();
 		} );
 
 	}
 
-	public function ScanContent($content) {
+	public function scan_content($content, $settings) {
 
 		$str = $content;
 		$content_array = array();
-		$p_content = "";
+		$p_content = ""; // 'p' means paragraph ( <p> tags).
 		$other_content = "";
 		$collect_p_mode = false;
-		$is_a_tag = false;
+		$is_a_tag = false; // 'a' means anchors ( <a> tags).
 		$is_inside_p_tag = false;
 
+
+		// Loop through "content" (DOM as string of current page) and find all paragraph (p tags),
+		// excluding: a tag that wraps p tags, or a tag that are inside p tags.
 		for ($i = 0; $i < strlen($str); $i++){
 
 			$current_char = $str[$i];
@@ -134,8 +69,9 @@ class Seo_Linker
 			
 			if($current_char === "<") {
 				if ($collect_p_mode) { 
-					// Finished collecting current <p> string, convert and save it:
-					$content_array[] = str_replace("ipsum", "<a href='/' >nachman</a>", $p_content); // replace before inserting to array |  
+					// Finished collecting *current* <p> string content,
+					// before inserting it to array of content, replace it with a link: 
+					$content_array[] = $this->replace_string($p_content, $settings); 
 					$p_content = "";
 				} 
 				$collect_p_mode = false;
@@ -152,6 +88,7 @@ class Seo_Linker
 
 			if($is_inside_p_tag && $current_char === ">" && !$is_a_tag ){
 				$collect_p_mode = true;
+				// Finished collecting *none* <p> string:
 				$content_array[] = $other_content; 
 				$other_content = "";
 			} 
@@ -161,14 +98,113 @@ class Seo_Linker
 			}
 		}
 		
-		echo implode('', $content_array);
-		// echo htmlentities(implode('', $content_array));
+		// Re-build the DOM string content from the array of content:
+		$new_content = implode('', $content_array);
+		return $new_content;
 	}
 
+	
+	public function replace_string($string, $settings) {
 
-	public function ReplaceStrings($string, $underline) {
+		$underline_style = "style='text-decoration: var(--seo_linker_underline_exist); text-decoration-style: var(--seo_linker_underline_type); text-decoration-color: var(--seo_linker_underline_color)'";
+	
+		foreach ($settings['seo_linker'] as $value) {
 
+			$word_to_search = $value['text'];
+			$url_to_link = $value['link']['url'];
+			$link_is_external = $value['link']['is_external'];
+
+			$output = "<a href='" . $url_to_link . "'";
+			if ($link_is_external === "on") {
+				$output .=  " target='_blank'";
+			}
+			$output .= " " . $underline_style . ">" . $word_to_search . "</a>";
+			$string = str_replace($word_to_search, $output, $string);
+		}
+
+		return $string;
 	}
-
-
 }
+
+
+
+// _____________________________________________________
+
+
+// $xml_content = simplexml_load_string($content);
+			// print_r($xml_content);
+			
+			// $str = htmlentities($content);
+			
+			
+			// $bla = $this->MyHelper("bla");
+
+			// echo "<br><hr><br>";
+			// echo $bla;
+			
+			
+			
+			// echo "<br><hr><br>";
+			// // echo $p_content;
+			// // echo htmlentities($p_content);
+			// // print_r(htmlentities($content_array));
+			// foreach ($content_array as $key => $value) {
+				// 	echo htmlentities($value);
+				// 	echo "<br><hr><br>";
+				// }
+				
+				// return  htmlentities($content);
+				
+				
+				// foreach ($content_array as $value) {
+		// 	echo htmlentities($value);
+		// 	echo "<br><hr><br>";
+		// }
+
+		
+		
+			// $dom = new \DomDocument();
+			// libxml_use_internal_errors(true);
+			
+			// $dom->loadHTML($content);
+			// $xpath = new \DOMXpath($dom);
+			
+			// $elements = $xpath->query('//p');
+			
+			// foreach ($elements as $entry) {
+				// 	echo "<br>>";
+				// 	print_r($entry->childNode);
+				// 	echo "<br><hr><br>";
+				// }
+				
+				// print_r($xpath->query('//p'));
+				// var_dump($xpath->query('//p'));
+				
+			// // $elements = $xpath->query('//p');
+			// // var_dump($elements);
+			
+			// libxml_clear_errors();
+
+			
+
+			// var_dump($content);
+			// content of specific page
+			// // print_r($settings['underline_exist']);
+
+			// ________________________________________________
+			// print_r($settings['seo_linker'][0]['text']);
+			// echo "<br>";
+			// print_r($settings['seo_linker'][0]['link']['url']);
+			// echo "<br> is external: ";
+			// print_r($settings['seo_linker'][0]['link']['is_external']);
+			// echo "<br>";
+			// print_r($settings['seo_linker'][0]['link']['nofollow']);
+			// echo "<br> underline_exist: ";
+			// print_r($settings['underline_exist']);
+			// echo "<br>";
+			// print_r($settings['underline_type']);
+			// echo "<br>";
+			// print_r($settings['exclude_pages']);
+			// echo "<br>";
+			
+			// echo "<br>";
--- modules/seo-text-linker/settings-site-seo-text-linker.php
+++ modules/seo-text-linker/settings-site-seo-text-linker.php
@@ -40,7 +40,7 @@ class Settings_Site_Seo_Text_Linker extends Tab_Base {
 		$this->start_controls_section(
 			'link_section_' . $this->get_id(),
 			[
-				'label' => esc_html__( 'Text Linker', 'elementor' ),
+				'label' => esc_html__( 'Edit Text To Link', 'elementor' ),
 				'tab' => $this->get_id(),
 			]
 		);
@@ -53,26 +53,21 @@ class Settings_Site_Seo_Text_Linker extends Tab_Base {
 				'label' => esc_html__( 'Search For Text', 'elementor' ),
 				'type' => Controls_Manager::TEXT,
 				'label_block' => true,
-				'required' => true,
 			]
 		);
 
 		$repeater->add_control(
 			'link',
 			[
-				'label' => esc_html__( 'Link', 'elementor' ),
-				'placeholder' => esc_html__( 'https://your-link.com', 'elementor' ),
+				'label' => esc_html__( 'Link URL', 'elementor' ),
+				'placeholder' => esc_html__( 'EXAMPLE: https://your-link.com', 'elementor' ),
 				'type' => Controls_Manager::URL,
 				'label_block' => true,
-				'required' => true,
-				'dynamic' => [
-					'active' => true,
-				],
 			]
 		);
 
 		$this->add_control(
-			'seo_repeater',
+			'seo_linker',
 			[
 				'label' => esc_html__( 'Items To Link', 'elementor' ),
 				'type' => Controls_Manager::REPEATER,
@@ -94,14 +89,14 @@ class Settings_Site_Seo_Text_Linker extends Tab_Base {
 			[
 				'label' => esc_html__( 'Edit Underline', 'elementor' ),
 				'tab' => $this->get_id(),
-				//				'tab' => Controls_Manager::TAB_CONTENT,
 			]
 		);
 
 		$this->add_control(
-			'underline_exist',
+			'seo_linker_underline_exist',
 			[
 				'label' => esc_html__( 'Choose Underline State', 'elementor' ),
+				'label_block' => true,
 				'type' => Controls_Manager::SELECT,
 				'options' => [
 					'none' => [
@@ -111,15 +106,19 @@ class Settings_Site_Seo_Text_Linker extends Tab_Base {
 						'title' => esc_html__( 'Underline', 'elementor' ),
 					],
 				],
+				'selectors' => [
+					'{{WRAPPER}}' => '--seo_linker_underline_exist: {{VALUE}};',
+				 ],
 				'default' => 'underline',
 				'toggle' => false,
 			]
 		);
 
 		$this->add_control(
-			'underline_type',
+			'seo_linker_underline_type',
 			[
 				'label' => esc_html__( 'Choose Underline Type', 'elementor' ),
+				'label_block' => true,
 				'type' => Controls_Manager::SELECT,
 				'options' => [
 					'solid' => [
@@ -138,13 +137,53 @@ class Settings_Site_Seo_Text_Linker extends Tab_Base {
 						'title' => esc_html__( 'Wavy', 'elementor' ),
 					],
 				],
+				'selectors' => [
+					'{{WRAPPER}}' => '--seo_linker_underline_type: {{VALUE}};',
+				 ],
 				'condition' => [
-					'underline_exist[value]' => 'underline',
+					'seo_linker_underline_exist[value]' => 'underline',
+				],
+			]
+		);
+
+		$this->add_control(
+			'seo_linker_underline_color',
+			[
+				'label' => esc_html__( 'Choose Underline Color', 'elementor' ),
+				'label_block' => true,
+				'type' => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}}' => '--seo_linker_underline_color: {{VALUE}};',
+				],
+				'condition' => [
+					'seo_linker_underline_exist[value]' => 'underline',
 				],
 			]
 		);
 
 		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'exclude_section' . $this->get_id(),
+			[
+				'label' => esc_html__( 'Exclude Pages', 'elementor' ),
+				'tab' => $this->get_id(),
+			]
+		);
+
+		$this->add_control(
+			'seo_linker_exclude_pages',
+			[
+				'label' => esc_html__( 'Insert Pages IDs To Exclude From Linker', 'elementor' ),
+				'label_block' => true,
+				'type' => Controls_Manager::TEXT,
+				'placeholder' => esc_html__( 'EX: 15,16,45', 'elementor' ),
+			]
+		);
+
+		$this->end_controls_section();
+
+		
 	}
 
 	public function on_save( $data ) {
@@ -152,5 +191,25 @@ class Settings_Site_Seo_Text_Linker extends Tab_Base {
 //		if ( isset( $data['settings']['site_favicon'] ) ) {
 //			update_option( 'site_icon', $data['settings']['site_favicon']['id'] );
 //		}
+
 	}
 }
+
+
+// 'description' => esc_html__( 'The `theme-color` meta tag will only be available in supported browsers and devices.', 'elementor' ),
+
+// $this->add_control(
+// 	$current_section['section'] . '_schemes_notice',
+// 	[
+// 		'name' => $current_section['section'] . '_schemes_notice',
+// 		'type' => Controls_Manager::RAW_HTML,
+// 		'raw' => sprintf(
+// 			/* translators: 1: Link open tag, 2: Link close tag. */
+// 			esc_html__( 'In order for Theme Style to affect all relevant Elementor elements, please disable Default Colors and Fonts from the %1$sSettings Page%2$s.', 'elementor' ),
+// 			'<a href="' . esc_url( Settings::get_url() ) . '" target="_blank">',
+// 			'</a>'
+// 		),
+// 		'content_classes' => 'elementor-panel-alert elementor-panel-alert-warning',
+// 		'render_type' => 'ui',
+// 	]
+// );
